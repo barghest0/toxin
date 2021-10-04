1d8a488a7e0435223a84cf64966c900a708117f6
barghest0 <bolshov03@mail.ru>
2021-10-04 17:17:51 +0300

    Made and styled active dropdowns

--- src/js/uikit/input-dropdown.js
+++ src/js/uikit/input-dropdown.js
@@ -1,28 +1,47 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min'
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
 
-$('.iqdropdown').iqDropdown({
+$('.guests').iqDropdown({
     maxItems: 20,
     setSelectionText: (itemCount, totalItems) => {
         if (totalItems == 0) {
             return `Сколько гостей`
         } else if (totalItems[totalItems.length - 1] == 1 || totalItems == 1) {
             return `${
-                itemCount['guests-adult'] +
-                itemCount['guests-child'] +
-                itemCount['guests-newborns']
+                itemCount['adult'] + itemCount['child'] + itemCount['newborns']
             } Гость`
         } else if (totalItems > 1 && totalItems < 5) {
             return `${
-                itemCount['guests-adult'] +
-                itemCount['guests-child'] +
-                itemCount['guests-newborns']
+                itemCount['adult'] + itemCount['child'] + itemCount['newborns']
             } Гостя`
         }
         return `${
-            itemCount['guests-adult'] +
-            itemCount['guests-child'] +
-            itemCount['guests-newborns']
+            itemCount['adult'] + itemCount['child'] + itemCount['newborns']
         } Гостей`
+    },
+})
+
+$('.apartments').iqDropdown({
+    maxItems: 20,
+    setSelectionText: (itemCount, totalItems) => {
+        console.log(itemCount)
+        if (totalItems == 0) {
+            return `Сколько комнат`
+        }
+        let res = []
+        for (const key in itemCount) {
+            if (itemCount[key] >= 1 && key == 'bedrooms') {
+                res.push(`${itemCount[key]} Комнат`)
+            } else if (itemCount[key] >= 1 && key == 'beds') {
+                res.push(`${itemCount[key]} Кроватей`)
+            } else if (itemCount[key] >= 1 && key == 'bathrooms') {
+                res.push(`${itemCount[key]} Ванных комнат`)
+            }
+        }
+        res = res.join(', ').slice(0, 20)
+        if (res.length == 20) {
+            return res + '...'
+        }
+        return res
     },
 })
