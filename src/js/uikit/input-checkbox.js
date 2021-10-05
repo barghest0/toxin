e392b4f97cb21ad5c31ff3a42a366f98985c44aa
barghest0 <bolshov03@mail.ru>
2021-10-05 17:27:39 +0300

    Finished the form elements page(without pagination), added some existing options

--- src/js/uikit/input-checkbox.js
+++ src/js/uikit/input-checkbox.js
@@ -1,3 +1,11 @@
-$('.label_checkbox-list').on('click', () => {
-    $('.input-selection_checkbox-list').toggleClass('active')
+$('.label_checkbox-list').on('click', function () {
+    $(this).toggleClass('active')
+    if ($(this).hasClass('active')) {
+        $(`.label_checkbox-list`)
+            .filter('.active')
+            .next()
+            .css({ maxHeight: '100%' })
+    } else {
+        $(this).siblings('.input-selection').css({ maxHeight: '0' })
+    }
 })
