3d57a921afc21ee7b53e266793c0ad2c209e4d03
barghest <bolshov03@mail.ru>
2021-10-26 12:08:58 +0300

    Minor improvements in SearchRoom page

--- src/js/uikit/input-checkbox.js
+++ src/js/uikit/input-checkbox.js
@@ -1,11 +1,3 @@
 $('.label_checkbox-list').on('click', function () {
-    $(this).toggleClass('active')
-    if ($(this).hasClass('active')) {
-        $(`.label_checkbox-list`)
-            .filter('.active')
-            .next()
-            .css({ maxHeight: '100%' })
-    } else {
-        $(this).siblings('.input-selection').css({ maxHeight: '0' })
-    }
+    $(this).next().toggleClass('active')
 })
