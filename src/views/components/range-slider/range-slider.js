287d3b59517b822b5f3e79784f3945f666a0fa54
barghest <bolshov03@mail.ru>
2022-02-08 17:32:58 +0000

    Add the range slider to project, but his broken

--- src/views/components/range-slider/range-slider.js
+++ src/views/components/range-slider/range-slider.js
@@ -1,2 +1,9 @@
-// import Slider from '../../../modules/range-slider/Slider.js'
-// import '../../../modules/range-slider/Slider.css'
+import Slider from '../../../libs/range-slider/range-slider';
+import '../../../libs/range-slider/range-slider.css';
+
+const slider = new Slider('.slider', {
+	hasTips: false,
+	hasScale: false,
+	onChange: data => console.log(data.value),
+});
+
