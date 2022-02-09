3cdc68d00c6e8f6bc1d317031d546ea44aaab84b
barghest <bolshov03@mail.ru>
2022-02-09 10:57:44 +0000

    Styled the slider

--- src/views/components/range-slider/range-slider.js
+++ src/views/components/range-slider/range-slider.js
@@ -1,8 +1,17 @@
 import Slider from '../../../libs/range-slider/range-slider';
 import '../../../libs/range-slider/range-slider.css';
 
+const handleChange = data => {
+	console.log(data.value);
+	$('.range-slider__start-price').html(data.value[0].toFixed() + ' ₽');
+	$('.range-slider__end-price').html(data.value[1].toFixed() + ' ₽');
+};
 const slider = new Slider('.slider', {
 	hasTips: false,
 	hasScale: false,
-	onChange: data => console.log(data.value),
+	isRange: true,
+	min: 0,
+	max: 15000,
+	step: 100,
+	onChange: handleChange,
 });
