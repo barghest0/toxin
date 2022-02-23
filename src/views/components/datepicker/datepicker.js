188cd0ad9d78fb9787299e9e36e80c4027126f0a
barghest <bolshov03@mail.ru>
2022-02-23 17:26:01 +0000

    Add buttons to datepicker

--- src/views/components/datepicker/datepicker.js
+++ src/views/components/datepicker/datepicker.js
@@ -1,13 +1,23 @@
 import AirDatepicker from "air-datepicker";
 import "air-datepicker/air-datepicker.css";
 
+const applyButton = {
+	content: "Применить",
+	className: "air-datepicker-button-apply",
+};
+
+const clearButton = {
+	content: "Очистить",
+	className: "air-datepicker-button-clear",
+};
+
 $(".range-date").each(function (index) {
 	const datepicker = this.lastChild.firstChild;
 	new AirDatepicker(datepicker, {
 		inline: true,
 		range: true,
 		minDate: new Date(),
-		buttons: ["today", "clear"],
+		buttons: [clearButton, applyButton],
 		onSelect: data => {
 			const [from, to] = data.formattedDate;
 			$(`#date-from-${index + 1}`).val(from);
@@ -22,6 +32,8 @@ $(".filter-date").each(function (index) {
 		inline: true,
 		range: true,
 		minDate: new Date(),
+		dateFormat: "d MMM",
+		buttons: [clearButton, applyButton],
 		onSelect: data => {
 			const [from, to] = data.formattedDate;
 			$(".text-field__input_filter-date").val(`${from} - ${to || ""}`);
