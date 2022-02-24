55068b0968c4ee9e0ca234b8c7904cd8956a2c02
barghest <bolshov03@mail.ru>
2022-02-24 10:50:24 +0000

    Add facade for datepickers

---
+++ src/js/Datepicker.js
@@ -0,0 +1,79 @@
+import AirDatepicker from "air-datepicker";
+import "air-datepicker/air-datepicker.css";
+
+class Datepicker {
+	constructor(targetElement, type, size) {
+		this.targetElement = targetElement;
+		this.type = type;
+		this.params = {};
+		this.size = size;
+		this.init();
+	}
+
+	init() {
+		this.params.buttons = [];
+		this.setBaseParams();
+		this.createClearButton();
+		this.createApplyButton();
+		this.createDatepicker();
+	}
+
+	createDatepicker() {
+		this.targetElement.each(index => {
+			const datepicker = this.targetElement.find(".datepicker")[index];
+			if (this.type === "filter") this.attachFilterOnSelect();
+			else this.attachRangeOnSelect(index);
+			new AirDatepicker(datepicker, this.params);
+		});
+		$(".air-datepicker-button").attr("type", "button");
+	}
+
+	setBaseParams() {
+		this.params.inline = true;
+		this.params.range = true;
+		this.params.minDate = new Date();
+		this.params.onSelect = e => console.log(e);
+		if (this.size === "md") {
+			this.params.classes = "air-datepicker_md";
+		}
+		if (this.type === "filter") {
+			this.params.dateFormat = "d MMM";
+		}
+	}
+
+	createClearButton() {
+		this.params.buttons.push("clear");
+	}
+
+	createApplyButton() {
+		const applyButton = {
+			content: "Применить",
+			className: "air-datepicker-button-apply",
+			onClick: e => {
+				e.$el.classList.toggle("active");
+			},
+		};
+		this.params.buttons.push(applyButton);
+	}
+
+	attachFilterOnSelect() {
+		this.params.onSelect = data => {
+			const [from, to] = data.formattedDate;
+			if (from && to) {
+				$("#filter-date").val(`${from} - ${to}`);
+			} else {
+				$("#filter-date").val(`Выберите дату`);
+			}
+		};
+	}
+
+	attachRangeOnSelect(index) {
+		this.params.onSelect = data => {
+			const [from, to] = data.formattedDate;
+			$(`#date-from-${index + 1}`).val(from);
+			$(`#date-to-${index + 1}`).val(to);
+		};
+	}
+}
+
+export default Datepicker;
--- src/views/components/datepicker/datepicker.js
+++ src/views/components/datepicker/datepicker.js
@@ -1,43 +1,27 @@
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
+import Datepicker from "../../../js/Datepicker";
 
-const applyButton = {
-	content: "Применить",
-	className: "air-datepicker-button-apply",
-};
+new Datepicker($(".range-date"), "range", "lg");
 
-const clearButton = {
-	content: "Очистить",
-	className: "air-datepicker-button-clear",
-};
+new Datepicker($(".filter-date"), "filter", "md");
 
-$(".range-date").each(function (index) {
-	const datepicker = this.lastChild.firstChild;
-	new AirDatepicker(datepicker, {
-		inline: true,
-		range: true,
-		minDate: new Date(),
-		buttons: [clearButton, applyButton],
-		onSelect: data => {
-			const [from, to] = data.formattedDate;
-			$(`#date-from-${index + 1}`).val(from);
-			$(`#date-to-${index + 1}`).val(to);
-		},
-	});
-});
+// $(".filter-date").each(function (index) {
+// 	const datepicker = this.lastChild.firstChild;
+// 	new AirDatepicker(datepicker, {
+// 		inline: true,
+// 		range: true,
+// 		minDate: new Date(),
+// 		dateFormat: "d MMM",
+// 		buttons: ["clear", applyButton],
+// 		onSelect: data => {
+// 			const [from, to] = data.formattedDate;
+// 			if (from && to) {
+// 				$("#filter-date").val(`${from} - ${to}`);
+// 			} else {
+// 				$("#filter-date").val(`Выберите дату`);
+// 			}
+// 		},
+// 		classes: "air-datepicker_md",
+// 	});
+// });
 
-$(".filter-date").each(function (index) {
-	const datepicker = this.lastChild.firstChild;
-	new AirDatepicker(datepicker, {
-		inline: true,
-		range: true,
-		minDate: new Date(),
-		dateFormat: "d MMM",
-		buttons: [clearButton, applyButton],
-		onSelect: data => {
-			const [from, to] = data.formattedDate;
-			$(".text-field__input_filter-date").val(`${from} - ${to || ""}`);
-		},
-		classes: "air-datepicker_md",
-	});
-});
+// $(".air-datepicker-button").attr("type", "button");
