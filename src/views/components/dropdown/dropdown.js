b0df1185e76a51492cbd3ba04c3014377f6b0c15
barghest <bolshov03@mail.ru>
2022-02-26 15:32:14 +0000

    Remove iqdropdown from the project

--- src/views/components/dropdown/dropdown.js
+++ src/views/components/dropdown/dropdown.js
@@ -1,8 +1,8 @@
-import "item-quantity-dropdown/lib/item-quantity-dropdown.min";
-import "item-quantity-dropdown/lib/item-quantity-dropdown.min.css";
-import { APARTMENTS_TYPE, LG_SiZE, MD_SIZE, PEOPLE_TYPE } from "../../../js/constants/dropdown";
-import Dropdown from "../../../js/Dropdown";
-
-new Dropdown($(".iqdropdown_guests"), LG_SiZE, PEOPLE_TYPE);
-
-new Dropdown($(".iqdropdown_apartments"), MD_SIZE, APARTMENTS_TYPE);
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
+import {
+  APARTMENTS_TYPE,
+  LG_SiZE,
+  MD_SIZE,
+  PEOPLE_TYPE,
+} from '../../../js/constants/dropdown';
