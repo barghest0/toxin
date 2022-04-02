03ae249f4a6756d55e227918f136a5126d9b2df5
barghest <bolshov03@mail.ru>
2022-04-02 23:33:06 +0000

    Fix responsive

--- src/views/components/range-slider/range-slider.js
+++ src/views/components/range-slider/range-slider.js
@@ -1,5 +1,4 @@
 import { RANGE_SLIDER_SELECTOR } from './constants';
 import RangeSlider from './RangeSliderModule';
 
-
-new RangeSlider(RANGE_SLIDER_SELECTOR);
+// new RangeSlider(RANGE_SLIDER_SELECTOR);
