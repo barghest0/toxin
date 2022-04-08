082923b0fba14f081b9bef04a7ba0448390a60d2
barghest <bolshov03@mail.ru>
2022-04-08 10:27:35 +0000

    Fix responsive filters in search room

--- src/views/pages/search-room/FilterButtonModule.js
+++ src/views/pages/search-room/FilterButtonModule.js
@@ -1,4 +1,4 @@
-import { BACKGROUND_SELECTOR, OPENED_CLASS } from './constants';
+import { BACKGROUND_SELECTOR, CLOSE_BUTTON_SELECTOR, OPENED_CLASS } from './constants';
 
 class FilterButton {
   constructor($button) {
@@ -8,6 +8,7 @@ class FilterButton {
 
   #init() {
     this.#setBackground();
+    this.#setCloseButton();
     this.#attachListeners();
   }
 
@@ -15,9 +16,14 @@ class FilterButton {
     this.$background = this.$button.siblings(BACKGROUND_SELECTOR);
   }
 
+  #setCloseButton() {
+    this.$closeButton = this.$background.find(CLOSE_BUTTON_SELECTOR);
+  }
+
   #attachListeners() {
     this.$button.on('click', this.#handleButtonClick.bind(this));
     this.$background.on('click', this.#handleBackgroundClick.bind(this));
+    this.$closeButton.on('click', this.#closeButtonClick.bind(this));
   }
 
   #handleButtonClick() {
@@ -28,6 +34,10 @@ class FilterButton {
     if (event.target.classList.contains(OPENED_CLASS)) {
       this.$background.removeClass(OPENED_CLASS);
     }
+  }
+
+  #closeButtonClick() {
+    this.$background.toggleClass(OPENED_CLASS);
   }
 }
 export default FilterButton;
--- src/views/pages/search-room/constants.js
+++ src/views/pages/search-room/constants.js
@@ -1,6 +1,12 @@
 const BACKGROUND_SELECTOR = '.search-room__filters';
-const FILTER_BUTTON_SELECTOR = '.js-search-room__filter-icon';
+const FILTER_BUTTON_SELECTOR = '.js-search-room__filters-icon';
+const CLOSE_BUTTON_SELECTOR = '.js-search-room__filters-close';
 
 const OPENED_CLASS = 'active';
 
-export { BACKGROUND_SELECTOR, OPENED_CLASS, FILTER_BUTTON_SELECTOR };
+export {
+  BACKGROUND_SELECTOR,
+  OPENED_CLASS,
+  FILTER_BUTTON_SELECTOR,
+  CLOSE_BUTTON_SELECTOR,
+};
