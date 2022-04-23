eec95569a4a70a6a18630ca87c683f130d9a76fb
barghest <bolshov03@mail.ru>
2022-04-23 18:32:52 +0000

    Change card block to scss mixin

--- src/libs/air-datepicker/constants.js
+++ src/libs/air-datepicker/constants.js
@@ -1,5 +1,5 @@
 const APPLY_BUTTON_CLASS = 'air-datepicker-button-apply';
 const BUTTON_SELECTOR = '.air-datepicker-button';
-const OPENED_CLASS = 'open';
+const OPENED_CLASS = 'datepicker_opened';
 
 export { APPLY_BUTTON_CLASS, OPENED_CLASS, BUTTON_SELECTOR };
