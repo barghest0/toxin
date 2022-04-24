ffab4e6afa141e3ef9929a9fcff852e7833770c6
barghest <bolshov03@mail.ru>
2022-04-24 12:49:20 +0000

    Fix state modifier in range and filter date #2

--- src/views/components/datepicker/Datepicker.js
+++ src/views/components/datepicker/Datepicker.js
@@ -6,8 +6,10 @@ import {
   DATE_FROM_SELECTOR,
   DATE_TO_SELECTOR,
   FILTER_DATE_SELECTOR,
-  OPENED_CLASS,
   SINGLE_DATEPICKER,
+  DATEPICKER_OPENED_CLASS,
+  RANGE_DATE_OPENED_CLASS,
+  FILTER_DATE_OPENED_CLASS,
 } from './constants';
 
 class Datepicker {
@@ -18,6 +20,7 @@ class Datepicker {
 
   init() {
     this.setInputs();
+    this.setParents();
     this.setDatepicker();
     this.attachInputsListeners();
     this.attachDocumentListener();
@@ -34,9 +37,14 @@ class Datepicker {
     this.$dateTo = this.$container.find(DATE_TO_SELECTOR);
     const $filterDate = this.$container.find(FILTER_DATE_SELECTOR);
 
-    if (!this.$dateTo.length) {
+    if (!this.isRageDate()) {
       this.$dateFrom = $filterDate;
     }
+  }
+
+  setParents() {
+    this.$dateFromParent = this.$dateFrom.parent();
+    this.$dateToParent = this.$dateTo.parent();
   }
 
   setDatepicker() {
@@ -45,27 +53,42 @@ class Datepicker {
   }
 
   attachInputsListeners() {
-    this.$dateFrom.on('click', this.openDatepickerAfterInputClick);
-    if (this.$dateTo) {
-      this.$dateTo.on('click', this.openDatepickerAfterInputClick);
+    this.$dateFrom.on('click', this.handleInputClick);
+    if (this.isRageDate()) {
+      this.$dateTo.on('click', this.handleInputClick);
     }
   }
 
+  isRageDate() {
+    return this.$dateTo.length;
+  }
+
   @boundMethod
-  openDatepickerAfterInputClick() {
-    this.datepicker.classList.toggle(OPENED_CLASS);
-    this.$container.toggleClass(OPENED_CLASS);
+  handleInputClick() {
+    this.datepicker.classList.toggle(DATEPICKER_OPENED_CLASS);
+    if (this.isRageDate()) {
+      this.$dateFromParent.toggleClass(RANGE_DATE_OPENED_CLASS);
+      this.$dateToParent.toggleClass(RANGE_DATE_OPENED_CLASS);
+    } else {
+      this.$dateFromParent.toggleClass(FILTER_DATE_OPENED_CLASS);
+    }
   }
 
   attachDocumentListener() {
-    document.addEventListener('click', this.closeDatepickerAfterDocumentClick);
+    document.addEventListener('click', this.handleDocumentClick);
   }
 
   @boundMethod
-  closeDatepickerAfterDocumentClick(e) {
-    if (!e.target.closest(CONTAINER_SELECTOR)) {
-      this.datepicker.classList.remove(OPENED_CLASS);
-      this.$container.removeClass(OPENED_CLASS);
+  handleDocumentClick(event) {
+    const isClickedOnDatepicker = event.target.closest(CONTAINER_SELECTOR);
+    if (!isClickedOnDatepicker) {
+      this.datepicker.classList.remove(DATEPICKER_OPENED_CLASS);
+      if (this.isRageDate()) {
+        this.$dateFromParent.removeClass(RANGE_DATE_OPENED_CLASS);
+        this.$dateToParent.removeClass(RANGE_DATE_OPENED_CLASS);
+      } else {
+        this.$dateFromParent.removeClass(FILTER_DATE_OPENED_CLASS);
+      }
     }
   }
 }
--- src/views/components/datepicker/constants.js
+++ src/views/components/datepicker/constants.js
@@ -1,20 +1,24 @@
-const CONTAINER_SELECTOR = '.js-datepicker-container';
+const CONTAINER_SELECTOR = '[data-datepicker="true"]';
 const DATEPICKER_SELECTOR = '.js-datepicker';
 
 const SINGLE_DATEPICKER = 0;
 
-const OPENED_CLASS = 'datepicker_opened';
+const DATEPICKER_OPENED_CLASS = 'datepicker_opened';
+const RANGE_DATE_OPENED_CLASS = 'range-date__field_opened';
+const FILTER_DATE_OPENED_CLASS = 'filter-date__field_opened';
 
 const DATE_FROM_SELECTOR = '.js-range-date__input_from';
 const DATE_TO_SELECTOR = '.js-range-date__input_to';
 const FILTER_DATE_SELECTOR = '.js-filter-date__input';
 
 export {
-  OPENED_CLASS,
   DATEPICKER_SELECTOR,
   DATE_FROM_SELECTOR,
   DATE_TO_SELECTOR,
   FILTER_DATE_SELECTOR,
   SINGLE_DATEPICKER,
   CONTAINER_SELECTOR,
+  DATEPICKER_OPENED_CLASS,
+  RANGE_DATE_OPENED_CLASS,
+  FILTER_DATE_OPENED_CLASS,
 };
