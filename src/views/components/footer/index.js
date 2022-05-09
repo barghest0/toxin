01995170f755ba0034ab9346c34e2406dec08dea
barghest <bolshov03@mail.ru>
2022-05-09 21:01:54 +0000

    Add social component #5

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,6 +1,3 @@
-import './images/facebook.svg';
-import './images/instagram.svg';
-import './images/twitter.svg';
-
 import '../logo';
 import '../subscribe-text-field';
+import '../social';
---
+++ src/views/components/social/index.js
@@ -0,0 +1,3 @@
+import './images/facebook.svg';
+import './images/instagram.svg';
+import './images/twitter.svg';
