e747ca77b4423741f698e4b360508957141e79be
barghest <bolshov03@mail.ru>
2022-06-24 10:29:30 +0000

    Remove useless components wrappers #10

--- src/views/components/range-slider/constants.js
+++ src/views/components/range-slider/constants.js
@@ -1,6 +1,6 @@
 const START_PRICE_SELECTOR = '.js-range-slider__start-price';
 const END_PRICE_SELECTOR = '.js-range-slider__end-price';
-const RANGE_SLIDER_SELECTOR = '.js-slider';
+const RANGE_SLIDER_SELECTOR = '.js-range-slider__slider';
 const RANGE_SLIDER_CONTAINER_SELECTOR = '.range-slider';
 
 export {
